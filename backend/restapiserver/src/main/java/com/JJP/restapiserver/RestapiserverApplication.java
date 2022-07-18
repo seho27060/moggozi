0c89fde521f4742122a58b1ed2d126126c5a748e
조성민 <chob5864@gmail.com>
2022-07-18 22:44:40 +0900

    build: 기본 빌드 코드 제공

---
+++ backend/restapiserver/src/main/java/com/JJP/restapiserver/RestapiserverApplication.java
@@ -0,0 +1,13 @@
+package com.JJP.restapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiserverApplication.class, args);
+	}
+
+}
---
+++ backend/restapiserver/src/test/java/com/JJP/restapiserver/RestapiserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JJP.restapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
