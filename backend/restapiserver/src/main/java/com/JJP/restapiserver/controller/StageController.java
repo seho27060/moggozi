624fe88304635e0466bfc16e74968c79d683ae52
박상현 <nomzaxs@gmail.com>
2022-07-28 10:55:27 +0900

    [S07P12C201-193] [BE-박상현] feat : stage detail rest api

--- backend/restapiserver/src/main/java/com/JJP/restapiserver/controller/StageController.java
+++ backend/restapiserver/src/main/java/com/JJP/restapiserver/controller/StageController.java
@@ -1,5 +1,6 @@
 package com.JJP.restapiserver.controller;
 
+import com.JJP.restapiserver.domain.dto.StageResponseDto;
 import com.JJP.restapiserver.domain.dto.StageSaveRequestDto;
 import com.JJP.restapiserver.domain.entity.stage.Stage;
 import com.JJP.restapiserver.service.StageService;
@@ -9,23 +10,34 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequiredArgsConstructor
-@RestController
+@RequestMapping("/stage")
+@RestController()
 public class StageController {
 
     private final StageService stageService;
 
-    @GetMapping("/stage/{challenge_id}")
+    @GetMapping("/{challenge_id}")
     public List<Stage> findById(@PathVariable Long challenge_id){
         return stageService.getStageList(challenge_id);
     }
 
-    @PostMapping("/stage/{challenge_id}")
+    @PostMapping("/{challenge_id}")
     public Long save(@PathVariable Long challenge_id, @RequestBody StageSaveRequestDto stageSaveRequestDto){
         return stageService.saveStage(challenge_id, stageSaveRequestDto);
     }
 
-    @GetMapping("/test")
-    public String test(){
-        return "test";
+    @GetMapping("/detail/{stage_id}")
+    public StageResponseDto stageDetail(@PathVariable Long stage_id){
+        return stageService.getStageDetail(stage_id);
+    }
+
+    @PutMapping("/{stage_id}")
+    public Long modify(@PathVariable Long stage_id){
+        return null;
+    }
+
+    @DeleteMapping("/{stage_id}")
+    public Long delete(@PathVariable Long stage_id){
+        return null;
     }
 }
