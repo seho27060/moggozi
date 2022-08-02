4b00a6ac68dde3ba13667e67c2bbca60de0f70bb
조성민 <chob5864@gmail.com>
2022-08-03 00:54:55 +0900

    [No issue] [BE-조성민] feat: 스웨거 테스트 시 jwt 토근 입력 기능 추가

--- backend/restapiserver/src/main/java/com/JJP/restapiserver/config/SwaggerConfig.java
+++ backend/restapiserver/src/main/java/com/JJP/restapiserver/config/SwaggerConfig.java
@@ -3,7 +3,10 @@ package com.JJP.restapiserver.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +18,7 @@ public class SwaggerConfig {
         return GroupedOpenApi.builder()
                 .group("v1-definition")
                 .pathsToMatch("/**")
+                .addOpenApiCustomiser(buildSecurityOpenApi())
                 .build();
     }
     @Bean
@@ -25,4 +29,15 @@ public class SwaggerConfig {
                         .version("v.1.0"));
 
     }
+    public OpenApiCustomiser buildSecurityOpenApi() {
+        // jwt token 을 한번 설정하면 header 에 값을 넣어주는 코드, 자세한건 아래에 추가적으로 설명할 예정
+        return OpenApi -> OpenApi.addSecurityItem(new SecurityRequirement().addList("jwt token"))
+                .getComponents().addSecuritySchemes("jwt token", new SecurityScheme()
+                        .name("Authorization")
+                        .type(SecurityScheme.Type.HTTP)
+                        .in(SecurityScheme.In.HEADER)
+                        .bearerFormat("JWT")
+                        .scheme("bearer"));
+    }
+
 }
