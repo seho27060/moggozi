cdc8f29c737348fbdacc11d4acaa9aff79742da2
Heojaeyoung <gj30347896@gmail.com>
2022-08-03 16:48:29 +0900

    [S07P12C201-258] [FE-허재영] fix: refreshToken 버그 수정

--- frontend/src/lib/refresh.ts
+++ frontend/src/lib/refresh.ts
@@ -1,48 +1,60 @@
 // JWT token refresh작업을 하는 곳.
 // (accessToken 과 토큰 만료시간은 로컬 스토리지, refreshToken 은 httpOnly cookie 에 저장되어 있다고 가정합니다.)
 
-
 import axios, { AxiosRequestConfig } from "axios";
 import Cookie from "js-cookie";
 // import { RefreshTokenResponse } from 'util/types/Response';
-import { apiConfig } from "../config"
+import { apiConfig } from "../config";
 import moment from "moment";
 
-const refresh = async (config: AxiosRequestConfig): Promise<AxiosRequestConfig> => {
+const refreshT = async (option: object) => {
+  const { data } = await axios.post(
+    `${apiConfig.apiRoot}/user/refreshToken`,
+    option
+  );
+  return data;
+};
+
+const refresh = async (
+  config: AxiosRequestConfig
+): Promise<AxiosRequestConfig> => {
   const refreshToken = Cookie.get("refreshToken");
   const expireAt = sessionStorage.getItem("expiresAt");
   let token = sessionStorage.getItem("accessToken");
 
   // 토큰이 만료되었고, refreshToken 이 저장되어 있을 때 => 토큰 만료 응답이 왔을 경우로 바꿔야 함
   if (moment(expireAt).diff(moment()) < 0 && refreshToken) {
-    const body = {
+    const option = {
       refreshToken: refreshToken,
     };
 
-    // 토큰 갱신 서버통신 // 토큰 갱신 주소 수정 필요.
-    const { data } = await axios.post(`${apiConfig.apiRoot}/user/refreshToken`, body);
-
-    token = data.accessToken;
-    sessionStorage.setItem("accessToken", data.accessToken);
-    sessionStorage.setItem(
-      "expiresAt",
-      moment().add(1, "hour").format("yyyy-MM-DD HH:mm:ss")
-    );
-    Cookie.remove("refreshToken");
-    Cookie.set("refreshToken", data.refreshToken, { secure: true });
+    refreshT(option)
+      .then((res) => {
+        token = res.accessToken;
+        sessionStorage.setItem("accessToken", res.accessToken);
+        sessionStorage.setItem(
+          "expiresAt",
+          moment().add(1, "hour").format("yyyy-MM-DD HH:mm:ss")
+        );
+        Cookie.remove("refreshToken");
+        Cookie.set("refreshToken", res.refreshToken, { secure: true });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
-  
   if (!config?.headers) {
-    throw new Error(`Expected 'config' and 'config.headers' not to be undefined`);
+    throw new Error(
+      `Expected 'config' and 'config.headers' not to be undefined`
+    );
   }
 
   config.headers["Authorization"] = `Bearer ${token}`;
-
   return config;
 };
 
 const refreshErrorHandle = (err: any) => {
-  Cookie.remove("refreshToken")
-}
+  Cookie.remove("refreshToken");
+};
 
-export { refresh, refreshErrorHandle }
+export { refresh, refreshErrorHandle };
--- frontend/src/lib/withTokenApi.ts
+++ frontend/src/lib/withTokenApi.ts
@@ -4,10 +4,6 @@ import { refresh, refreshErrorHandle } from "./refresh"
 
 const withTokenApi = axios.create({
     baseURL: apiConfig.apiRoot,
-    headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`
-    },
     timeout: 10000,
     params: {},
 });
@@ -16,7 +12,7 @@ withTokenApi.interceptors.request.use(refresh, refreshErrorHandle)
 
 export default withTokenApi;
 
-// 로그인 상태 유지
+// 계정 관련
 export const persistAuth = async () => {
     const { data } = await withTokenApi.get(`/user/myinfo`)
     return data
@@ -29,6 +25,11 @@ export const userDetail = async () => {
 
 export const logoutApi = async () => {
     const { data } = await withTokenApi.post('/user/logout')
+    return data
+}
+
+export const updateUserApi = async (userId: number | null, option: object) => {
+    const { data } = await withTokenApi.post(`user/update/${userId}`, option)
     return data
 }
 
