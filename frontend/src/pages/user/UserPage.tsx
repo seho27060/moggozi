af2572ba63e039f3cfd39da455b72556789cc23d
HeoJaeyoung <gj3034@naver.com>
2022-08-04 09:50:52 +0900

    hotfix: user/userPage 코드 중복 에러 수정

--- frontend/src/pages/user/UserPage.tsx
+++ frontend/src/pages/user/UserPage.tsx
@@ -3,7 +3,6 @@
 
 import { Link } from "react-router-dom";
 import { useState } from "react"
-import { Link } from "react-router-dom"
 
 import { userDetail } from "../../lib/withTokenApi"
 function UserPage() {
