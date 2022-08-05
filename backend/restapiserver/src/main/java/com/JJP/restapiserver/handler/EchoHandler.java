e4d0201ad1bff188821b8a53250139d7ae5d850d
조성민 <chob5864@gmail.com>
2022-08-05 16:22:41 +0900

    [No issue] [BE-조성민] fix : builder 변수명 변경

--- backend/restapiserver/src/main/java/com/JJP/restapiserver/handler/EchoHandler.java
+++ backend/restapiserver/src/main/java/com/JJP/restapiserver/handler/EchoHandler.java
@@ -151,7 +151,7 @@ public class EchoHandler extends TextWebSocketHandler {
                 .message(msg)
                 .sequence(index)
                 .type(type)
-                .check(0)
+                .read(0)
                 .build();
         alert = alertRepository.save(alert);
         return AlertResponseDto.builder()
