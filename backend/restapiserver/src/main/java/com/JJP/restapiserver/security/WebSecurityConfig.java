c2c3bc6b1b475fedf55c61b40ae838ddff55315c
junghojin <hojinnjung@naver.com>
2022-08-09 02:57:05 +0900

    [No issue] [BE-정호진] chore: CORS TEST

--- backend/restapiserver/src/main/java/com/JJP/restapiserver/security/WebSecurityConfig.java
+++ backend/restapiserver/src/main/java/com/JJP/restapiserver/security/WebSecurityConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
@@ -62,7 +61,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
-                .requestMatchers(request -> CorsUtils.isPreFlightRequest(request)).permitAll()
+//                .requestMatchers(request -> CorsUtils.isPreFlightRequest(request)).permitAll()
                 .antMatchers("/**").permitAll()
 //                .anyRequest().permitAll()
                 .and()
