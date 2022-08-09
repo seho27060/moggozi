376dc1fb9b85b5ae784632333049fb06e15b2af6
조성민 <chob5864@gmail.com>
2022-08-09 19:12:27 +0900

    [No issue] [BE-조성민] feat : 태그로 챌린지 검색 페이징 구현 추가

--- backend/restapiserver/src/main/java/com/JJP/restapiserver/controller/challenge/ChallengeController.java
+++ backend/restapiserver/src/main/java/com/JJP/restapiserver/controller/challenge/ChallengeController.java
@@ -2,11 +2,16 @@ package com.JJP.restapiserver.controller.challenge;
 
 
 import com.JJP.restapiserver.domain.dto.challenge.*;
+import com.JJP.restapiserver.domain.entity.Tag.ChallengeTag;
 import com.JJP.restapiserver.domain.entity.challenge.Challenge;
+import com.JJP.restapiserver.repository.Tag.ChallengeTagRepository;
 import com.JJP.restapiserver.repository.challenge.JoinedChallengeRepository;
 import com.JJP.restapiserver.security.JwtUtils;
+import com.JJP.restapiserver.service.Tag.ChallengeTagService;
 import com.JJP.restapiserver.service.challenge.ChallengeService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -23,29 +28,40 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChallengeController {
 
+    private Logger logger = LoggerFactory.getLogger(ChallengeController.class);
     private final ChallengeService challengeService;
 
     private final JoinedChallengeRepository joinedChallengeRepository;
     @Autowired
     private final JwtUtils jwtUtils;
 
+    private final ChallengeTagService challengeTagService;
+
+
     @GetMapping("/hobby/{hobby}")
     public ResponseEntity getChallengeListByHobby(@PathVariable("hobby") String hobby,
                                                   HttpServletRequest request){
         Long user_id = jwtUtils.getUserIdFromJwtToken(request.getHeader("Authorization"));
         List<ChallengeListResponseDto> challengeList = challengeService.getChallengeListByHobby(hobby, user_id);
-        return new ResponseEntity<List<ChallengeListResponseDto>>(challengeList,HttpStatus.OK);
+        return new ResponseEntity<>(challengeList,HttpStatus.OK);
     }
 
+    // challenge/search/식물?page=1&size=2
     @GetMapping("/search/{keyword}")
     public ResponseEntity getChallengeListByKeyword(@PathVariable("keyword") String keyword, Pageable pageable,
                                                     HttpServletRequest request)
     {
-        Long member_id = jwtUtils.getUserIdFromJwtToken(request.getHeader("Authorization"));
-
-        ChallengePageDto challengeList = challengeService.getChallengeListByKeyword(keyword,
+        if(request.getHeader("Authorization") != null){
+            Long member_id = jwtUtils.getUserIdFromJwtToken(request.getHeader("Authorization"));
+            ChallengePageDto challengeList = challengeService.getChallengeListByKeyword(keyword,
                 pageable, member_id);
-        return new ResponseEntity(challengeList, HttpStatus.OK);
+            return new ResponseEntity(challengeList, HttpStatus.OK);
+        }
+        else{
+            ChallengePageDto challengeList = challengeService.getChallengeListByKeyword(keyword,
+                    pageable);
+            return new ResponseEntity(challengeList, HttpStatus.OK);
+        }
     }
 
     @GetMapping("/rank")
@@ -116,6 +132,25 @@ public class ChallengeController {
         Long num = joinedChallengeRepository.countByMember_id(member_id);
         System.out.println(num);
         return new ResponseEntity<List>(joinedChallengeRepository.findTop8ByMember_idOrderByModifiedDateDesc(member_id), HttpStatus.OK);
+    }
+
+    @GetMapping("/tag/search/{keyword}")
+    public ResponseEntity getChallengeContainingTag(@PathVariable String keyword, Pageable pageable,
+                                                    HttpServletRequest request){
+        logger.debug("-----------컨트롤러 시작---------------");
+        List<ChallengeListResponseDto> challengeListResponseDtoList = null;
+        if(request.getHeader("Authorization") != null){
+            challengeListResponseDtoList = challengeService.getChallengeContainingTag(keyword,
+                    pageable);
+        }
+        else{
+        Long member_id = jwtUtils.getUserIdFromJwtToken(request.getHeader("Authorization"));
+
+        challengeListResponseDtoList = challengeService.getChallengeContainingTag(keyword,
+                pageable, member_id);
+        }
+        logger.debug("------------컨트롤러 종료---------------");
+        return new ResponseEntity(challengeListResponseDtoList, HttpStatus.OK);
     }
 
 }
