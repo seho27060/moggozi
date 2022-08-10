f21709f03c666405c76140db5414da918755d064
junghojin <hojinnjung@naver.com>
2022-08-11 01:49:11 +0900

    [No issue] [BE-정호진] fix: SNS 로그인 회원가입 오류
    **수정사항**
    - SNS 로그인 회원가입 오류

--- backend/restapiserver/src/main/java/com/JJP/restapiserver/domain/entity/member/Member.java
+++ backend/restapiserver/src/main/java/com/JJP/restapiserver/domain/entity/member/Member.java
@@ -65,7 +65,7 @@ public class Member {
     private ERole role;
 */
 
-    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "role_id", referencedColumnName = "role_id")
     private Role role;
 
