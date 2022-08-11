9ce168cc633b3c5c34425af81233671ad61e350d
Heojaeyoung <gj30347896@gmail.com>
2022-08-11 23:54:12 +0900

    [S07P12C201-279] [FE-허재영] style: 회원가입 완료 페이지 스타일링

--- frontend/src/pages/accounts/CompleteSignUp.tsx
+++ frontend/src/pages/accounts/CompleteSignUp.tsx
@@ -1,11 +1,20 @@
-import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
+import styles from "./CompleteSignUp.module.scss";
+import RamG from "./../../asset/RamG.png";
 
 const CompleteSignUp = () => {
-    return <div>
-      회원가입이 완료되었습니다.
-      <Link to={"/"}>메인화면으로 이동 </Link>
-    </div>
-}
+  const navigate = useNavigate();
 
-export default CompleteSignUp
+  return (
+    <div className={styles.complete}>
+        <img src={RamG} alt="logo" />
+        <div>환영합니다!</div>
+        <div>모꼬지 회원가입이 완료되었습니다.</div>
+        <div>챌린지를 통해 취미를 실천하고 공유해보세요!</div>
+        <button onClick={() => {navigate("/account/login")}}>로그인 페이지로 이동</button>
+    </div>
+  );
+};
+
+export default CompleteSignUp;
