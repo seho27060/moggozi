7c151099926e44778b15aa37a849c3444e621cb8
박상현 <nomzaxs@gmail.com>
2022-08-13 01:51:38 +0900

    [BE-박상현] fix : post Repository 수정

--- backend/restapiserver/src/main/java/com/JJP/restapiserver/domain/entity/stage/Post.java
+++ backend/restapiserver/src/main/java/com/JJP/restapiserver/domain/entity/stage/Post.java
@@ -41,6 +41,8 @@ public class Post extends BaseTimeEntity {
     @Column(length = 300)
     private String postImg;
 
+    private int state;
+
 
     // 포스트 좋아요와 다대일 양방향 관계
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
