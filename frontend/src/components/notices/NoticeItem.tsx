30e45c8c5f0b5e3a084360c633e0992b484a2858
Heojaeyoung <gj30347896@gmail.com>
2022-08-14 22:04:26 +0900

    [No issue] [FE-허재영] style: 공지사항 스타일링 수정

--- frontend/src/components/notices/NoticeItem.tsx
+++ frontend/src/components/notices/NoticeItem.tsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 import { Notice } from "../../store/notice";
 
-import styles from "./NoticeItem.module.scss"
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import styles from "./NoticeItem.module.scss";
 const NoticeItem: React.FC<{ notice: Notice }> = ({ notice }) => {
   const options = {
     weekday: "long",
@@ -11,18 +12,18 @@ const NoticeItem: React.FC<{ notice: Notice }> = ({ notice }) => {
   };
   // console.log(notice)/
   return (
-    <div className={styles.container}>
-      <Link to={`/notice/detail/${notice.noticeId}`} className={styles.noticeTitle}>
-        <div >{notice.title}</div>
+      <Link to={`/notice/detail/${notice.noticeId}`} className={styles.container} >
+
+            <div>{notice.title}</div>
+            <div className={styles.noticeDate}>
+              <div>{new Date(notice!.modifiedDate!).toLocaleDateString("ko-Kr", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}{" "}
+              | 관리자</div><div><ChevronRightIcon /></div>
+            </div>
       </Link>
-      <div className={styles.noticeDate}>
-        {new Date(notice!.modifiedDate!).toLocaleDateString("ko-Kr", {
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-        })}  |  관리자
-      </div>
-    </div>
   );
 };
 
