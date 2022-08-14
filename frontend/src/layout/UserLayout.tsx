a7943223f7e616070bda248b8400d92fbaac9c38
Heojaeyoung <gj30347896@gmail.com>
2022-08-14 18:01:54 +0900

    [No issue] [FE-허재영] style: footer 생성

--- frontend/src/layout/UserLayout.tsx
+++ frontend/src/layout/UserLayout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import NavigationBar from "./NavigationBar";
-
+import Footer from "../components/ui/Footer";
 const UserLayout = () => {
   return (
     <div>
-      <NavigationBar />
-      UserLayout
-      <Outlet />
+      <div style={{ minHeight: "100%", position: "relative"}}>
+        <NavigationBar />
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   )
 }
