e910c4f01cb82d47fdb67025c7df0bb8934bda11
YunH <yunh04131@gmail.com>
2022-08-16 02:18:54 +0900

    [No Issue] [FE-임윤혁] fix: 취미태그 빈 값 제출 막고, 제출되면 초기화되게 변경

--- frontend/src/components/challenge/HobbyForm.tsx
+++ frontend/src/components/challenge/HobbyForm.tsx
@@ -8,29 +8,32 @@ import Modal from "../ui/Modal";
 
 import styles from "./HobbyForm.module.scss";
 
-
 const HobbyForm: React.FC = () => {
   const dispatch = useDispatch();
   const hobbyInputRef = useRef<HTMLInputElement>(null);
   const [dropDownList, setDropDownList] = useState<Hobby[]>([]); // 자동완성 기능을 위한 dropDownList
   const hobbyList = useSelector((state: RootState) => state.hobby.hobbyList);
-
-  const [ content, setContent ] = useState(<div></div>)
-  const [ modalOpen, setModalOpen ] = useState(false);
+  const [content, setContent] = useState(<div></div>);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [inputText, setInputText] = useState("");
 
   const closeModal = () => {
     document.body.style.overflow = "unset";
-    setModalOpen(false)
-  }
+    setModalOpen(false);
+  };
 
-  function submitHandler(event: React.FormEvent) {
-    event.preventDefault();
+  function submitHandler() {
     const enteredHobby = hobbyInputRef.current!.value;
-    console.log(hobbyList)
-    if (hobbyList.length  >= 5) {
-      setContent(<div>태그는 5개까지 등록 가능합니다.</div>)
-      setModalOpen(true)
-      return
+    console.log(hobbyList);
+    if (!enteredHobby) {
+      setContent(<div>태그를 입력해주세요!</div>);
+      setModalOpen(true);
+      return;
+    }
+    if (hobbyList.length >= 5) {
+      setContent(<div>태그는 5개까지 등록 가능합니다.</div>);
+      setModalOpen(true);
+      return;
     }
     hobbyExist(enteredHobby)
       .then((res) => {
@@ -43,33 +46,41 @@ const HobbyForm: React.FC = () => {
             setHobby({ name: enteredHobby }) // 새로운 취미 DB에 등록 후 가져오기
               .then((res) => {
                 dispatch(addHobby(res));
-              })
-              .catch((err) => {
-                console.log(err.response);
+                setInputText("");
               });
           }
         } else {
-          setHobby({ name: enteredHobby })
-            .then((res) => {
-              // Form에서 등록한 취미인지 확인
-              if (hobbyList.some((hobby) => hobby.id === res.id)) {
-                alert("이미 존재하는 취미입니다.");
-              } else {
-                dispatch(addHobby(res));
-              }
-            })
-            .catch((err) => {
-              console.log(err.response);
-            });
+          setHobby({ name: enteredHobby }).then((res) => {
+            // Form에서 등록한 취미인지 확인
+            if (hobbyList.some((hobby) => hobby.id === res.id)) {
+              setContent(<div>이미 존재하는 취미입니다.</div>);
+              setModalOpen(true);
+            } else {
+              dispatch(addHobby(res));
+              setInputText("");
+            }
+          });
         }
       })
       .catch((err) => {
         console.log(err.response);
       });
   }
-
-  function changeInputHandler(event: React.ChangeEvent) {
+  const onClickHandler = (event: React.MouseEvent) => {
     event.preventDefault();
+    submitHandler();
+  };
+
+  const onKeyUpHandler = (event: React.KeyboardEvent) => {
+    event.preventDefault();
+    if (event.key === "Enter") {
+      submitHandler();
+    }
+  };
+
+  function changeInputHandler(event: React.ChangeEvent<HTMLInputElement>) {
+    event.preventDefault();
+    setInputText(event.target.value);
     const enteredQuery = hobbyInputRef.current!.value;
     if (enteredQuery === "") {
       setDropDownList([]);
@@ -86,30 +97,31 @@ const HobbyForm: React.FC = () => {
 
   useEffect(() => {
     document.body.style.overflow = "unset";
-  }, [])
-
+  }, []);
 
   return (
     <div>
       <div className={styles.hobbyForm}>
         <label htmlFor="hobby">태그</label>
-          <input
-            type="text"
-            required
-            id="hobby"
-            ref={hobbyInputRef}
-            onChange={changeInputHandler}
-            autoComplete="off"
-          />
-          <button type="button" onClick={submitHandler}>
-            &#43;
-          </button>
+        <input
+          type="text"
+          required
+          id="hobby"
+          ref={hobbyInputRef}
+          onChange={changeInputHandler}
+          autoComplete="off"
+          onKeyUp={onKeyUpHandler}
+          value={inputText}
+        />
+        <button type="button" onClick={onClickHandler}>
+          &#43;
+        </button>
       </div>
       <div className={styles.tag}>
-      {dropDownList.length === 0 && <div >해당하는 단어가 없습니다.</div>}
-      {dropDownList.map((dropDownItem) => {
-        return <div># {dropDownItem.name}</ div>;
-      })}
+        {dropDownList.length === 0 && <div>해당하는 단어가 없습니다.</div>}
+        {dropDownList.map((dropDownItem) => {
+          return <div># {dropDownItem.name}</div>;
+        })}
       </div>
       <Modal open={modalOpen} close={closeModal} header="안내">
         {content}
