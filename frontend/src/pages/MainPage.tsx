c1515317ecd36e69c95a2788bcdceb8c45944583
YunH <yunh04131@gmail.com>
2022-08-15 10:20:13 +0900

    [No Issue] [FE-임윤혁] fix: 메인페이지 로딩 수정

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -30,15 +30,20 @@ import { setPostModalOpen, setPostUpdateFormState } from "../store/postModal";
 import { useDispatch } from "react-redux";
 import PostDetailItem from "../components/post/PostDetailItem";
 import PostUpdateForm from "../components/post/PostUpdateForm";
+import Loader from "../components/ui/Loader";
 
 const MainPage: React.FC = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
+  // 로딩
   const [rankIsLoading, setRankIsLoading] = useState(true);
   const [recentTryIsLoading, setRecentTryIsLoading] = useState(true);
   const [recentIsLoading, setRecentIsLoading] = useState(true);
   const [recommendIsLoading, setRecommendIsLoading] = useState(true);
+  const [rankPostIsLoading, setRankPostIsLoading] = useState(true);
+  const [recentPostIsLoading, setRecentPostIsLoading] = useState(true);
+
   const [recentPostList, setRecentPostList] = useState<PostData[]>([]);
   const [likePostList, setLikePostList] = useState<PostData[]>([]);
   const [loadedChallengeRankList, setLoadedChallengeRankList] = useState<
@@ -53,7 +58,7 @@ const MainPage: React.FC = () => {
     ChallengeItemState[]
   >([]);
 
-  const { postModalOpen, postFormModalOpen,postUpdateFormOpen} = useSelector(
+  const { postModalOpen, postUpdateFormOpen } = useSelector(
     (state: RootState) => state.postModal
   );
 
@@ -66,21 +71,32 @@ const MainPage: React.FC = () => {
     setRecentTryIsLoading(true);
     setRecentIsLoading(true);
     setRecommendIsLoading(true);
+    setRankPostIsLoading(true);
+    setRecentPostIsLoading(true);
+    // 최신 포스트
     fetchPostRecentList(0, 13)
       .then((res) => {
-        console.log("recent post call", res);
         setRecentPostList(res.content);
+        setRecentPostIsLoading(false);
       })
-      .catch((err) => console.log("recent post err", err));
+      .catch((err) => {
+        console.log("recent post err", err);
+        setRecentPostIsLoading(false);
+      });
+    // 인기 포스트
     fetchPostLikeList(0, 4)
       .then((res) => {
-        console.log("like post call", res);
         setLikePostList(res.content);
+        setRankPostIsLoading(false);
       })
-      .catch((err) => console.log("like post err", err));
+      .catch((err) => {
+        console.log("like post err", err);
+        setRankPostIsLoading(false);
+      });
 
     if (isLoggedIn) {
       // 로그인 한 경우
+      // 인기 순
       isLoginFetchChallengeRankList(0, 3)
         .then((res) => {
           setLoadedChallengeRankList(res.content);
@@ -90,7 +106,7 @@ const MainPage: React.FC = () => {
           console.log(err);
           setRankIsLoading(false);
         });
-
+      // 최신 순
       isLoginFetchRecentChallengeList(0, 2)
         .then((res) => {
           setLoadedRecentChallengeList(res.content);
@@ -100,7 +116,7 @@ const MainPage: React.FC = () => {
           console.log(err);
           setRecentIsLoading(false);
         });
-
+      // 추천 순
       isLoginFetchRecommendChallengeList()
         .then((res) => {
           const twoPickRecommend = res.slice(0, 2);
@@ -124,6 +140,7 @@ const MainPage: React.FC = () => {
         });
     } else {
       // 로그인 안 한 경우
+      // 인기 순
       fetchChallengeRankList(0, 3)
         .then((res) => {
           setLoadedChallengeRankList(res.content);
@@ -134,7 +151,7 @@ const MainPage: React.FC = () => {
           console.log(err);
           setRankIsLoading(false);
         });
-
+      // 최신 순
       fetchRecentChallengeList(0, 2)
         .then((res) => {
           setLoadedRecentChallengeList(res.content);
@@ -144,7 +161,7 @@ const MainPage: React.FC = () => {
           console.log(err);
           setRecentIsLoading(false);
         });
-
+      // 추천 순
       fetchRecommendChallengeList()
         .then((res) => {
           const twoPickRecommend = res.slice(0, 2);
@@ -157,17 +174,13 @@ const MainPage: React.FC = () => {
         });
     }
   }, [isLoggedIn]);
-  console.log(loadedChallengeRankList);
+
   return (
     <div className={styles.container}>
       <div className={styles.mainPage}>
         <MainSlider />
         {/* 최신 챌린지*/}
-        {recentIsLoading === true && (
-          <section>
-            <p>recentList Loading...</p>
-          </section>
-        )}
+        {recentIsLoading === true && <Loader />}
         {recentIsLoading === false && (
           <div className={styles.myChallenge}>
             <div className={styles.myTitle}>
@@ -179,11 +192,7 @@ const MainPage: React.FC = () => {
         )}
 
         {/* 추천 챌린지*/}
-        {recommendIsLoading === true && (
-          <section>
-            <p>recommendList Loading...</p>
-          </section>
-        )}
+        {recommendIsLoading === true && <Loader />}
         {recommendIsLoading === false && (
           <div className={styles.myChallenge}>
             <div className={styles.myTitle}>
@@ -195,11 +204,7 @@ const MainPage: React.FC = () => {
         )}
 
         {/* 참여한 챌린지 */}
-        {isLoggedIn === true && recentTryIsLoading === true && (
-          <section>
-            <p>MyTryList Loading...</p>
-          </section>
-        )}
+        {isLoggedIn === true && recentTryIsLoading === true && <Loader />}
         {isLoggedIn === true && recentTryIsLoading === false && (
           <div className={styles.myChallenge}>
             <div className={styles.myTitle}>
@@ -211,11 +216,7 @@ const MainPage: React.FC = () => {
         )}
 
         {/* 인기 챌린지 */}
-        {rankIsLoading === true && (
-          <section>
-            <p>RankList Loading...</p>
-          </section>
-        )}
+        {rankIsLoading === true && <Loader />}
         {rankIsLoading === false && (
           <div className={styles.popChallenge}>
             <div className={styles.title}>인기 챌린지</div>
@@ -223,42 +224,47 @@ const MainPage: React.FC = () => {
           </div>
         )}
 
-        {/*자동 슬라이더  */}
-        {[
-          [0, 4],
-          [4, 9],
-          [9, 13],
-        ].map((start) => (
-          <div style={{ minHeight: "13rem" }}>
-            <AutoPlaySlider rtl={start[1] - start[0] - 4}>
-              {recentPostList.slice(start[0], start[1]).map((post) => {
-                console.log(post);
-                return (
-                  <div key={post.id}>
-                    <img
-                      src={
-                        post.postImg!.length !== 0
-                          ? post.postImg[0].path!
-                          : "https://blog.kakaocdn.net/dn/vckff/btqCjeJmBHM/tMVpe4aUIMfH4nKS4aO3tK/img.jpg"
-                      }
-                      alt=""
-                      style={{ minWidth: "300px" }}
-                    ></img>
-                  </div>
-                );
-              })}
-            </AutoPlaySlider>
-          </div>
-        ))}
+        {/*최신 포스팅 자동 슬라이더  */}
+        {recentPostIsLoading === true && <Loader />}
+        {recentPostIsLoading === false &&
+          [
+            [0, 4],
+            [4, 9],
+            [9, 13],
+          ].map((start) => (
+            <div style={{ minHeight: "13rem" }}>
+              <AutoPlaySlider rtl={start[1] - start[0] - 4}>
+                {recentPostList.slice(start[0], start[1]).map((post) => {
+                  console.log(post);
+                  return (
+                    <div key={post.id}>
+                      <img
+                        src={
+                          post.postImg!.length !== 0
+                            ? post.postImg[0].path!
+                            : "https://blog.kakaocdn.net/dn/vckff/btqCjeJmBHM/tMVpe4aUIMfH4nKS4aO3tK/img.jpg"
+                        }
+                        alt=""
+                        style={{ minWidth: "300px" }}
+                      ></img>
+                    </div>
+                  );
+                })}
+              </AutoPlaySlider>
+            </div>
+          ))}
         <br />
         {/* 인기순 포스팅 */}
-        <Grid container spacing={1}>
-          {likePostList.map((post) => (
-            <Grid key={post.id} item xs={3}>
-              <MainPageItem post={post} />
-            </Grid>
-          ))}
-        </Grid>
+        {rankPostIsLoading === true && <Loader />}
+        {rankPostIsLoading === false && (
+          <Grid container spacing={1}>
+            {likePostList.map((post) => (
+              <Grid key={post.id} item xs={3}>
+                <MainPageItem post={post} />
+              </Grid>
+            ))}
+          </Grid>
+        )}
       </div>
       <div>
         {postModalOpen && (
